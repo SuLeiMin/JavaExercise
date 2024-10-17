82e3d5842d5242f2df12fbea60d62bfe4125ce02
suleimin <suleimin@gmail.com>
2024-10-17 23:19:26 +0900

    JavaSE Exercise

---
+++ src/basic/ConditionalStatement.java
@@ -0,0 +1,15 @@
+package basic;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+		
+		int age = 18;
+		
+		if(age >=18) {
+			System.out.println("can go university");
+		}
+
+	}
+
+}
